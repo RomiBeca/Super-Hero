ae8c38fca4004e2e86abebc8fcb678af67ee0669
Romina Beca <rominabeca.v@gmail.com>
2024-03-10 11:41:04 -0300

    estructura basica

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,23 @@
 console.log("vinculado")
 
+$(function () {
+
+    $.ajax({
+
+        url: "https://www.superheroapi.com/api.php/7246518678795864/1",
+        method: "GET",
+
+        success(data) {
+            console.log(data)
+        },
+        error(e) {
+            console.log(data)
+        }
+
+
+
+    })
+
+
+
+})
